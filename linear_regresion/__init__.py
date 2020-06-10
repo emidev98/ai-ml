3e480a37f2b8dc781707580e93f8701e6e731ee3
Emilian Harastasan <eharastasan@gmail.com>
2020-06-10 21:07:01 +0200

    FEAT_linear-regresion: new method to do it

--- linear_regresion/__init__.py
+++ linear_regresion/__init__.py
@@ -1,17 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def estimate_b0_b1(x, y):
+    n = np.size(x)
 
-if __name__ == '__init__':
-    x = np.array(list(range(0,9)))
-    y = np.array([1,2,3,5,4,6,8,7,9])
+    # calculate the average of x and y
+    m_x, m_y = np.mean(x), np.mean(y)
 
-    coefficient = np.polyfit(x, y, 1)
+    # Calculate the sum of xy and xx
+    sum_xy = np.sum((x - m_x) * (y - m_y))
+    sum_xx = np.sum(x * (x - m_x))
 
-    m = coefficient[0]
-    b = coefficient[1]
-    est_y = (m * x) + b
+    # calculate the coefficient of the regresion
+    b_1 = sum_xy / sum_xx
+    b_0 = m_y - (b_1 * m_x)
 
-    plt.plot(x, est_y)
-    plt.scatter(x, y)
+    return (b_0, b_1)
+
+def plot_regression(x, y, b):
+    plt.scatter(x, y, color = "g", marker = "o", s = 30)
+
+    y_pred = b[0] + (b[1] * x)
+
+    plt.plot(x, y_pred, color = "b")
+
+    plt.xlabel("X - Independent")
+    plt.xlabel("Y - Dependent")
+
     plt.show()
+
+
+if __name__ == '__main__':
+    x = np.array([1, 2, 3, 4, 5])
+    y = np.array([2, 3, 5, 6, 5])
+
+    b = estimate_b0_b1(x, y)
+    print(b)
+
+    plot_regression(x, y, b)
+
+    # coefficient = np.polyfit(x, y, 1)
+
+    # m = coefficient[0]
+    # b = coefficient[1]
+    # est_y = (m * x) + b
+
+    # plt.plot(x, est_y)
+    # plt.scatter(x, y)
+    # plt.show()
